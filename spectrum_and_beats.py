681ff361417ea5cc04c1e7326615b0741e4af283
indi <indivicivet@gmail.com>
2023-11-02 18:10:16 +0000

    do some rough bpm calculation and plotting

--- spectrum_and_beats.py
+++ spectrum_and_beats.py
@@ -1,13 +1,32 @@
+from pathlib import Path
+
 import matplotlib.pyplot as plt
+import numpy as np
 import librosa
 
-timeseries, sample_rate = librosa.load(librosa.ex("choice"), duration=10)
+TRACK_PATH = Path(__file__).parent / "music" / "ddhn.mp3"
+
+timeseries, sample_rate = librosa.load(TRACK_PATH)
 print(timeseries.shape)
 onset_env = librosa.onset.onset_strength(
     y=timeseries,
     sr=sample_rate,
 )
-plt.plot(librosa.times_like(onset_env, sr=sample_rate), 2000 * onset_env)
+times = librosa.times_like(onset_env, sr=sample_rate)
+
+WINDOW = 5  # samples
+onset_spikes = onset_env * (onset_env > (onset_env.mean() * 1.2))
+beat_times = []
+for i, (t, val) in enumerate(zip(times, onset_spikes)):
+    if val and val == onset_spikes[i - WINDOW:i + WINDOW].max():
+        beat_times.append(t)
+
+deltas = np.array([b - a for a, b in zip(beat_times, beat_times[1:])])
+bpms = 60 / deltas
+plt.plot(beat_times[:-1], bpms)
+plt.show()
+
+plt.plot(times, 4000 * onset_spikes)
 mel_spectrogram = librosa.feature.melspectrogram(
     y=timeseries,
     sr=sample_rate,
